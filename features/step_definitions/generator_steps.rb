23c25dc707b4843e17bbba0aed6008f60ebf74c1
danascheider <dscheider@renewfinancial.com>
2016-05-30 17:01:47 -0700

    Make initial feature pass

--- features/step_definitions/generator_steps.rb
+++ features/step_definitions/generator_steps.rb
@@ -5,3 +5,7 @@ end
 When(/^I run the XML data generator$/) do
   @output = XSDML.generate!(@schema)
 end
+
+Then(/^the output should match the schema$/) do
+  expect{ @schema.validate(@output) }.not_to raise_error(Nokogiri::XML::SyntaxError)
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,3 +3,4 @@ require "coveralls"
 Coveralls.wear! if ENV["COVERAGE"] == "true"
 
 require_relative "../../lib/xsdml"
+require "nokogiri"
