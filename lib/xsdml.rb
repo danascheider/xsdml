9ebbb7e37f79c57e99d447f82454a768369644a1
danascheider <dscheider@renewfinancial.com>
2016-05-30 21:13:32 -0700

    Make second RSpec pass

--- lib/xsdml.rb
+++ lib/xsdml.rb
@@ -1,2 +1,4 @@
+require "nokogiri"
+
 path = File.join(File.dirname(__FILE__), "**/*")
 Dir[path].each {|f| require f if f.match(/\.rb$/) }
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -4,5 +4,21 @@ RSpec.describe XSDML::Generator do
       str = "<?xml version=\"1.1\" encoding=\"UTF-8\" ?>"
       expect(described_class.generate!("foo")).to include(str)
     end
+
+    context "specific attributes" do
+      let(:raw_schema) do
+        '<?xml version="1.1" encoding="UTF-8" ?>
+        <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+          <xs:element name="foo" type="xs:string" />
+        </xs:schema>'
+      end
+
+      let(:schema) { Nokogiri::XML::Schema(raw_schema) }
+
+      it "generates matching XML" do
+        output = Nokogiri::XML(described_class.generate!(raw_schema))
+        expect(schema.valid?(output)).to be true
+      end
+    end
   end
 end
