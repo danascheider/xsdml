28204352b4a10932f755882c6b7869b5349c487d
danascheider <dscheider@renewfinancial.com>
2016-05-31 10:49:09 -0700

    Wrap up committing for today

--- spec/lib/xsdml/xml_schema_spec.rb
+++ spec/lib/xsdml/xml_schema_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe XSDML::XmlSchema do
+  subject { described_class.new(schema) }
 
   describe "#initialize" do
     let(:schema) do
@@ -9,8 +10,24 @@ RSpec.describe XSDML::XmlSchema do
     end
 
     it "initializes with a schema" do
-      sch = described_class.new(schema)
-      expect(sch.schema).to eql schema
+      expect(subject.schema).to eql schema
+    end
+  end
+
+  describe "#generate_example" do
+    describe "trivial examples" do
+      context "simple types" do
+        let(:raw_schema) do
+          '<?xml version="1.1" encoding="UTF-8" ?>
+          <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+            <xs:element name="foo" type="xs:string" />
+          </xs:schema>'
+        end
+
+        let(:output) do
+          '<foo></foo>'
+        end
+      end
     end
   end
 end
